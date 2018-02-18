a9aac01a46152fc90ac4dabc250c440327251e10
kevmcgovern <kevin.mcgovern92@gmail.com>
2018-02-17 17:02:07 -0800

    Adds /home route that grabs data from DB; finishes getClan() to actually send data back to view

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
 
+const db = require("./helpers/db.js");
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clan");
 
 const app = express();
@@ -14,7 +16,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  console.log('Does anything log?');
   res.sendStatus(200);
+});
+
+app.get('/home', (req, res) => {
+  console.log('Inside /home')
+  db.getClan(res);
 });
 
 var port = process.env.PORT || 5000;
